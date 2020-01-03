f8a71e4eb3986d2016a3fbe7ce333d7d97d7aa24
Mark Harfouche <mark.harfouche@gmail.com>
2020-01-03 12:02:03 -0500

    Fixup build process

--- recipe/build.sh
+++ recipe/build.sh
@@ -6,7 +6,11 @@ cp ${RECIPE_DIR}/libjpeg.pc.in ${SRC_DIR}/.
 aclocal
 libtoolize --force
 automake --add-missing
+# not sure why this file gets overwritten, but it is quite important
+# to have certain things defined here
 autoreconf
+# How to do this without a patch???
+patch jconfig.cfg ${RECIPE_DIR}/8d_define_cjpeg_djpeg.patch
 
 ./configure --prefix=$PREFIX \
             --build=${BUILD}\
@@ -15,6 +19,6 @@ autoreconf
             --enable-static=yes
 
 make -j${CPU_COUNT} ${VERBOSE_AT}
-# make check
+make check
 make install
 
